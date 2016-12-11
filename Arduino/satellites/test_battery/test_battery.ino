5be202975d3bc0379fcd2ad809e956323047b99f
f-r-y <f_r_y_@hotmail.com>
2016-12-11 19:14:37 +0100

    battery lib

--- Arduino/satellites/test_battery/test_battery.ino
+++ Arduino/satellites/test_battery/test_battery.ino
@@ -4,21 +4,12 @@
 #include <VirtualWire_Config.h>
 
 
+#include "battery-probe.h"
+
+
 rfcom rfcom_obj;
-char vcc_voltage_buf[6];
-long vcc_voltage;//readvcc
 
-
-
-// number of analog samples to take per reading
-#define NUM_SAMPLES 10
-
-int sum = 0;                    // sum of samples taken
-unsigned char sample_count = 0; // current sample number
-float voltage = 0.0;            // calculated voltage
 char voltage_buf[6];
-
-int battery_pin = A2;
 
 void setup() {
   //Serial.begin(9600);
@@ -33,29 +24,13 @@ void loop() {
   //dtostrf(vcc_voltage, 0, 1, vcc_voltage_buf);
   //rfcom_obj.send_message(String(vcc_voltage_buf));
 
-  
-  readVoltagePin();
+  float voltage = readBatteryVoltage();
   dtostrf(voltage, 0, 2, voltage_buf);
   rfcom_obj.send_message(String(voltage_buf));
   
   delay(5000);
 }
 
-void readVoltagePin() {
-  // take a number of analog samples and add them up
-    while (sample_count < NUM_SAMPLES) {
-        sum += analogRead(battery_pin);
-        sample_count++;
-        delay(10);
-    }
-    // calculate the voltage
-    // use 5.0 for a 5.0V ADC reference voltage
-    // 5.015V is the calibrated reference voltage
-    voltage = ((float)sum / (float)NUM_SAMPLES * 5.09) / 1024.0;
-
-    sample_count = 0;
-    sum = 0;
-}
 long readVcc() {
   // Read 1.1V reference against AVcc
   // set the reference to Vcc and the measurement to the internal 1.1V reference
